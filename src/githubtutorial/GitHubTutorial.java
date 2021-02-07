d58657e6b9cf568086244981f850d96f946bd24f
ZubearNassimi <nassimi@sheridanc.on.ca>
2021-02-07 02:53:19 -0500

    FirstCommitPractice

--- src/githubtutorial/GitHubTutorial.java
+++ src/githubtutorial/GitHubTutorial.java
@@ -36,7 +36,17 @@ public class GitHubTutorial {
                 click next. click to check to Master button and click next. and then click Finish. click yes for prompt
                 window.
         step 7. To PULL - right click on project, under git, under remote, click on PULL. then click Finish.
-        step 8. To Fetch and Merge - right click on project, to go git, go to remote, click on fetch.
+        step 8. To Fetch and Merge - right click on project, to go git, go to remote, click on fetch. At prompt window
+                click NO. then right click on project, under git, under Diff, click on DIFF TO HEAD. on DIFF drop down
+                menu choose origin/master. Here you can see the changes to the code. if you accept the changes, right
+                click on projec, under git, under Branch/Tag click on Merge Revision. click on Select. Under branches,
+                under remote - click on origin/master. on the right side choose the top most version (most recent).
+                select it and click select.
+        step 9. To Branch - right click on project, under git, under branch/tag, then click on Create Branch. Name your
+                Branch. then Click Create. Now work on the branch and make changes. save and commit when done.
+                Go to Git Repository Browser. you can see from the diamond icon whether you are working on the master
+                code or a branch of the code. double click on master and click on checkout. now you have switched back
+                to master code.
                 
         */
     
